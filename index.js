449488d9db0179780f39415726476d70aad2e99d
MARTIN SABINO PALMA <23569348@comunidadort.edu.ar>
2025-04-26 11:38:31 -0300

    modifica rutas a rutas relativas

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import {leerArchivoComoString, escribirTextoEnArchivo} from './src/utils/fileUti
 import {transformarStringEnArrayDeNumeros, transformarArrayDeNumerosAUnSoloString} from './src/utils/transformUtils.js';
 import {combinarDosArrays, combinarNArrays, combinarNArrays2} from './src/apareo.js';
 
-console.log(escribirTextoEnArchivo('C:/TP2/01-TP-ManejoDeArchivosSync/in/mensaje.txt', "--------Trabajo Practico N°1 Manejo de Archivos----------", true))
+console.log(escribirTextoEnArchivo('./in/mensaje.txt', "--------Trabajo Practico N°1 Manejo de Archivos----------", true))
 
 // leo los 4 archivos a memoria
-const archivo1=leerArchivoComoString('C:/TP2/01-TP-ManejoDeArchivosSync/in/10NumerosOrdenadosEntre1y50(setA).in')
-const archivo2=leerArchivoComoString('C:/TP2/01-TP-ManejoDeArchivosSync/in/10NumerosOrdenadosEntre1y50(setB).in')
-const archivo3=leerArchivoComoString('C:/TP2/01-TP-ManejoDeArchivosSync/in/imparesOrdenadosEntre1y999.in')
-const archivo4=leerArchivoComoString('C:/TP2/01-TP-ManejoDeArchivosSync/in/paresOrdenadosEntre2y1000.in')
+const archivo1=leerArchivoComoString('./in/10NumerosOrdenadosEntre1y50(setA).in')
+const archivo2=leerArchivoComoString('./in/10NumerosOrdenadosEntre1y50(setB).in')
+const archivo3=leerArchivoComoString('./in/imparesOrdenadosEntre1y999.in')
+const archivo4=leerArchivoComoString('./in/paresOrdenadosEntre2y1000.in')
 
 
 // preparo los 4 arrays a partir de los archivo leidos
@@ -18,7 +18,7 @@ const array3 = transformarStringEnArrayDeNumeros(archivo3, ',')
 const array4 = transformarStringEnArrayDeNumeros(archivo4, ',')
 
 // combino los primeros dos arrays
-escribirTextoEnArchivo('C:/TP2/01-TP-ManejoDeArchivosSync/out/combinado3.out',
+escribirTextoEnArchivo('./out/combinado3.out',
     transformarArrayDeNumerosAUnSoloString(combinarDosArrays(array1, array2), ','), true);
 
 // combino los cuatro arrays
@@ -27,7 +27,7 @@ let aCombinar= [array1, array2, array3, array4]
 
 
 
-escribirTextoEnArchivo('C:/TP2/01-TP-ManejoDeArchivosSync/out/combinado5.out',
+escribirTextoEnArchivo('./out/combinado5.out',
     transformarArrayDeNumerosAUnSoloString(combinarNArrays2(aCombinar), ','), true);
 
     console.log('fin TP')
